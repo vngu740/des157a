bd4aab78d780471b9fecac257f23f70081baf01d
Victoria <vngu@ucdavis.edu>
2024-03-08 14:39:57 -0800

    studio 3

--- studio3/script.js
+++ studio3/script.js
@@ -3,18 +3,30 @@
     console.log('reading JS');
 
     const startGame = document.querySelector('#startgame');
-    const gameControl = document.querySelector('#gamecontrol');
-    const game = document.querySelector('#game');
+    const gameControl = document.querySelector('#gamecontrol')
     const score = document.querySelector('#score');
     const actionArea = document.querySelector('#actions');
-    const teethbg = document.querySelector('#teeth');
+    const teethbg = document.querySelector('img');
+    const quit = document.querySelector('#quit');
+    const player1 = document.querySelector('#player1');
+    const player2 = document.querySelector('#player2');
+
+    /* Background music */
+    const bgmusic = new Audio('sounds/bgmusic.mp3');
+    bgmusic.play();
+    bgmusic.loop = true;
+
+    /* Audio effects */
+
+    const drill = new Audio('sounds/drill.mp3');
+    const scream = new Audio('sounds/scream.mp3');
 
 
     /* Game data */
 
     const gameData = {
-        dice: ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg', 'images/10.jpg', 'images/11.jpg', 'images/12.jpg'],
-        players: ['player 1', 'player 2'],
+        dice: ['images/1.png', 'images/2.png', 'images/3.png', 'images/4.png', 'images/5.png', 'images/6.png', 'images/7.png', 'images/8.png', 'images/9.png', 'images/10.png', 'images/11.png', 'images/12.png'],
+        players: ['Player 1', 'Player 2'],
         score: [0, 0],
         roll: 0,
         index: 0,
@@ -30,9 +42,8 @@
         gameData.index = Math.round(Math.random());
         console.log(gameData.index);
 
-
-        gameControl.innerHTML = '<h2>The Game Has Started</h2>'
-        gameControl.innerHTML += '<button id="quit">Wanna quit?</button>'
+        gameControl.innerHTML = ''
+        quit.innerHTML = '<button id="quitbutton">Wanna quit?</button>'
 
         document.querySelector('#quit').addEventListener('click', function () {
             location.reload();
@@ -47,11 +58,32 @@
     /* Sets up turn */
 
     function setUpTurn() {
-        game.innerHTML = `<p>Roll the dice for ${gameData.players[gameData.index]}</p>`;
 
-        actionArea.innerHTML = '<button id="roll">Break the teeth</button>'
+        actionArea.innerHTML = `<p>Break teeth for ${gameData.players[gameData.index]}</p>`;
+        actionArea.innerHTML += '<button id="roll">Break the teeth</button>'
+
+        /* Bounces character when it's their turn */
+        const char1turn = document.querySelector('#player1');
+        const char2turn = document.querySelector('#player2');
+
+        if (gameData.index == 0) {
+            char1turn.classList.add('playerturn');
+            char2turn.classList.remove('playerturn');
+        } else if (gameData.index == 1) {
+            char2turn.classList.add('playerturn');
+            char1turn.classList.remove('playerturn');
+        }
+        /* --- */
+
+        player1.innerHTML = '<p>Player 1</p>';
+        player1.innerHTML += '<img src="images/leftperson.png" height="35" />';
+
+        player2.innerHTML = '<p>Player 2</p>';
+        player2.innerHTML += '<img src="images/rightperson.png" height="35" />';
+
 
         document.querySelector('#roll').addEventListener('click', function () {
+            drill.play();
             throwDice();
 
         });
@@ -63,28 +95,27 @@
     /* Throws dice */
 
     function throwDice() {
-        actionArea.innerHTML = '';
         gameData.roll = Math.floor(Math.random() * 12) + 1;
-        game.innerHTML = `<p>Break teeth for ${gameData.players[gameData.index]}</p>`;
+        actionArea.innerHTML = `<p>Break teeth for ${gameData.players[gameData.index]}</p>`;
 
-        teethbg.style.backgroundImage = `url('${gameData.dice[gameData.roll - 1]}')`
-
+        teethbg.src = `${gameData.dice[gameData.roll - 1]}`
 
         if (gameData.roll === 1) {
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>Weak. Your minions only broke one tooth, switching to ${gameData.players[gameData.index]}.</p>`;
+            actionArea.innerHTML += `<p>Weak. Your minions only broke one tooth, switching to ${gameData.players[gameData.index]}.</p>`;
             setTimeout(setUpTurn, 2000);
         }
 
         else {
             gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.roll;
-            actionArea.innerHTML = '<button id="rollagain">Break more teeth</button> <button id="pass">No more please</button>';
+            actionArea.innerHTML += '<button id="pass">No more please</button> <button id="rollagain">Break more teeth</button>';
 
             document.querySelector('#rollagain').addEventListener('click', function () {
                 setUpTurn();
             });
 
             document.querySelector('#pass').addEventListener('click', function () {
+                scream.play();
                 gameData.index ? (gameData.index = 0) : (gameData.index = 1);
                 setUpTurn();
             });
@@ -99,13 +130,14 @@
 
     function checkWinningCondition() {
         if (gameData.score[gameData.index] > gameData.gameEnd) {
-            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins the game with ${gameData.score[gameData.index]}</h2>`;
-            teethbg.style.backgroundImage = `url(images/end.jpg)`
-            actionArea.innerHTML = '';
-            document.querySelector('#quit').innerHTML = 'Start a new game';
-        }
+            score.innerHTML = `<h2 class="winner"><img src="images/confetti.gif" height="75"> ${gameData.players[gameData.index]} wins the game with ${gameData.score[gameData.index]} 🦷 broken. <img src="images/confetti.gif" height="75"></h2>`;
 
-        else {
+            teethbg.src = 'images/end.png'
+
+            actionArea.innerHTML = '';
+            document.querySelector('#quit').innerHTML = '<button id="startagain">Start a new game</button>';
+        } else {
+            //show current score
             showCurrentScore();
         }
     };
@@ -115,7 +147,8 @@
     /* Shows current score */
 
     function showCurrentScore() {
-        score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]}: ${gameData.score[0]}</strong> and <strong>${gameData.players[1]}: ${gameData.score[1]}</strong></p>`
+        score.innerHTML = `<p id="score1">🦷 <strong>${gameData.score[0]}</strong></p>`
+        score.innerHTML += `<p id="score2">🦷 <strong>${gameData.score[1]}</strong></p>`
     };
 
 
